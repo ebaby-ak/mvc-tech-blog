3b8fd0f7fa97ebf12780220418fd3621c14f9890
ebaby-ak <166087675+ebaby-ak@users.noreply.github.com>
2024-08-19 21:19:15 -0500

    built out comment model

--- server.js
+++ server.js
@@ -10,9 +10,22 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: process.env.DB_SECRET,
+    cookie: {},
+    resave: false,
+    store: new SequelizeStore({
+        db: sequelize,
+    })
+};
+
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
