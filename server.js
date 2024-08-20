26a40099194776394d1cc5e113ba1b733afd9fde
ebaby-ak <166087675+ebaby-ak@users.noreply.github.com>
2024-08-19 21:06:32 -0500

    added to server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const exphbs = requre('express-handlebare');
+const routes = require('./controllers');
+const session = require('express-session');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync();
+
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+});
